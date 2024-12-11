a0c05f490fc8f31b12d5d440b1089fda6c7f8c4a
hvsun <hvsun@snack24h,com>
2024-12-11 16:45:15 +0900

    문제 업데이트

---
+++ src/src/ajshin/level_2/캐시.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+    LinkedList<String> cache = new LinkedList<>();
+    for (String city : cities) {
+      city = city.toLowerCase();
+      if (cache.contains(city)) {
+        cache.remove(city);
+        cache.addFirst(city);
+        answer += 1;
+      } else {
+        if (cache.size() == cacheSize && !cache.isEmpty()) {
+            cache.removeLast();
+        }
+        if (cacheSize != 0) {
+          cache.addFirst(city);
+        }
+        answer += 5;
+      }
+    }
+
+    return answer;
+    }
+}
